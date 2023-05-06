3d963ab79946692794d31ec3407659b56502b295
Hernán Marrapodi <hernanmarra2002@gmail.com>
2023-05-06 15:03:11 -0300

    Cambia a funcion acotada

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,9 +2,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 object Utils {
-    fun formatMonto(monto: Double?): String {
-        return String.format("%.2f", monto)
-    }
+    fun formatMonto(monto: Double?): String = String.format("%.2f", monto)
 
     fun mostrarDateComoCadena(fecha: Date): String {
         val formatoFecha = SimpleDateFormat("yyyy-MM-dd")
